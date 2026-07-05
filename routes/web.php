df8ded2a2c6d360ae414a4d98eb813ed1560ef55
Justin Bezanson <justinbezanson@gmail.com>
2026-07-05 20:28:17 -0300

    Fix formatting by adding newlines at the end of multiple test files and route definitions

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::inertia('/', 'Welcome')->name('home');
+// Route::inertia('/', 'Welcome')->name('home');
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
