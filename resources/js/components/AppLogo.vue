e1f044cf16db7aa7815690f7c204af09544b781e
Justin Bezanson <justinbezanson@gmail.com>
2026-07-07 20:01:59 -0300

    Remove commented-out navigation items from AppHeader and AppSidebar components

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -6,7 +6,7 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
     <div
         class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground"
     >
-        <AppLogoIcon class="size-full object-cover" />
+        <AppLogoIcon class="size-full object-cover  border-2 border-gray-700" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate leading-tight font-semibold"
